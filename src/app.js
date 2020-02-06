2663ba9b665b1aece406b25fd55971b30fdca4d8
Mathew, Anish K <anish.k.mathew@accenture.com>
2020-02-05 22:19:04 -0600

    payment code

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 const { accounts, users, writeJSON } = require('./data');
 
 const accountRoutes = require('./routes/accounts');
-const serviceRoutes = require('./routes/services');
+const servicesRoutes = require('./routes/services');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -16,8 +16,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/account', 'accountRoutes');
-app.use('/services', 'servicesRoutes');
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res) =>
   res.render('index', { title: 'Account Summary', accounts })
